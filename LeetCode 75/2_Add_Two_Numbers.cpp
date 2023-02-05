99ce4b7227b64fb03b096bf98dbf80ba9743d7e8
jap <javakanaya@gmail.com>
2023-02-05 19:03:10 +0700

    LeetCode : 2 (Debugging)

---
+++ LeetCode 75/2_Add_Two_Numbers.cpp
@@ -0,0 +1,50 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        ListNode *result = new ListNode(0);
+        ListNode *currNode = result;
+        int sum = 0;
+        while (l1 || l2)
+        {
+            if (l1)
+            {
+                sum += l1->val;
+                cout << "l1 " << sum << endl;
+                l1 = l1->next;
+            }
+            if (l2)
+            {
+                sum += l2->val;
+                cout << "l2 " << sum << endl;
+                l2 = l2->next;
+            }
+            cout << sum << endl;
+            currNode->next = new ListNode(sum % 10);
+            currNode = currNode->next;
+
+            if (sum >= 10)
+                sum = 1;
+            else
+                sum = 0;
+        }
+
+        return result->next;
+    }
+};
